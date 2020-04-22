b873a89cda13d95e43a68645533d50fb0100180b
valosekj <jan.valosek@upol.cz>
2020-04-22 15:40:10 +0200

    Head rotation implementation.

--- snake.py
+++ snake.py
@@ -8,20 +8,18 @@
 
 import pyglet
 from random import randint
+from pathlib import Path
 
 SQUARE_SIZE = [64, 64]      # tile size in px
 WINDOW_SIZE = [640, 640]    # window size in px
-green_path = 'images/green.png'
-apple_path = 'images/apple.png'
-# head_path = 'images/head.png'
-head_path = 'images/green.png'
-
+TILES_DIRECTORY = Path('snake-tiles')
 
 objects = list()            # list of sprite objects (snake parts and food)
 
 # create main window
 window = pyglet.window.Window(width=WINDOW_SIZE[0], height=WINDOW_SIZE[1])
 
+# TODO - snake has anchor in middle -> does not fit properly to game window
 
 def load_image(filename):
     """
@@ -30,14 +28,13 @@ def load_image(filename):
     :return: loaded image
     """
     image = pyglet.image.load(filename)
-    #image.anchor_x = image.width // 2
-    #image.anchor_y = image.height // 2
+    image.anchor_x = image.width // 2
+    image.anchor_y = image.height // 2
     return image
 
-
-green_image = load_image(green_path)
-apple_image = load_image(apple_path)
-head_image = load_image(head_path)
+snake_tiles = {}
+for path in TILES_DIRECTORY.glob('*.png'):
+    snake_tiles[path.stem] = load_image(path)
 
 # Define text properties for counter
 label = pyglet.text.Label(color=(255, 0, 0, 255), x=1/2*SQUARE_SIZE[0], y=1/2*SQUARE_SIZE[1], font_size=36)
@@ -48,7 +45,7 @@ class Snake:
 
     def __init__(self):
         self.speed = 1 / 4                                  # snake speed in seconds
-        self.snake_positions = [[3, 5], [3, 4], [3, 3]]     # initial snake
+        self.snake_positions = [[3, 5, 0], [3, 4, 0], [3, 3, 0]]     # initial snake
         self.direction = 'UP'                               # initial direction of snake movement
         self.score_counter = 0                              # initial score counter
         #self.apple_position = [7, 7]                       # initial food position
@@ -59,18 +56,22 @@ class Snake:
         Change snake position based on direction.
         Direction is changed by key press.
         """
-        new_head = [self.snake_positions[0][0], self.snake_positions[0][1]]
+        new_head = [self.snake_positions[0][0], self.snake_positions[0][1], self.snake_positions[0][2]]
         if self.direction == 'UP':
             new_head[1] += 1
+            new_head[2] = 0
         elif self.direction == 'DOWN':
             new_head[1] -= 1
+            new_head[2] = 180
         elif self.direction == 'RIGHT':
             new_head[0] += 1
+            new_head[2] = 90
         elif self.direction == 'LEFT':
             new_head[0] -= 1
+            new_head[2] = 270
 
         # Snake ate apple, so generate new apple positions and do not shorten snake
-        if new_head == self.apple_position:
+        if new_head[:2] == self.apple_position:
             self.place_apple()
             self.score_counter += 1       # increment score counter
             self.increase_speed()
@@ -79,13 +80,14 @@ class Snake:
                 WINDOW_SIZE[1] / SQUARE_SIZE[1]:
             self.print_end()
         # END game when snake hits itself
-        elif new_head in [i for i in self.snake_positions]:
+        elif new_head[:2] in [[x, y] for x, y, _ in self.snake_positions]:
             self.print_end()
         # Delete last element from snake
         else:
             self.snake_positions.pop()
 
         self.snake_positions.insert(0, new_head)
+        print(self.snake_positions)
 
     def place_apple(self):
         """
@@ -95,7 +97,7 @@ class Snake:
             self.apple_position = [randint(1, WINDOW_SIZE[0] / SQUARE_SIZE[0] - 1),
                                    randint(1, WINDOW_SIZE[1] / SQUARE_SIZE[1] - 1)]
             # if food is not in snake break loop
-            if self.apple_position not in [i for i in self.snake_positions]:
+            if self.apple_position not in [[x, y] for x, y, _ in self.snake_positions]:
                 break
 
     def increase_speed(self):
@@ -125,18 +127,20 @@ def show():
     batch = pyglet.graphics.Batch()
 
     # SNAKE's HEAD
-    objects.append(pyglet.sprite.Sprite(head_image, my_snake.snake_positions[0][0] * SQUARE_SIZE[0],
+    objects.append(pyglet.sprite.Sprite(snake_tiles['bottom-tongue'], my_snake.snake_positions[0][0] * SQUARE_SIZE[0],
                                         my_snake.snake_positions[0][1] * SQUARE_SIZE[1], batch=batch))
-    # TODO - add head rotation
-    #objects[0].rotation = 180
+    # head rotattion
+    objects[-1].rotation = my_snake.snake_positions[0][2]
+
+    # TODO - add body and tail rotation
 
     # SNAKE's BODY
-    for x, y in my_snake.snake_positions[1:]:
+    for x, y, _ in my_snake.snake_positions[1:]:
         # snake_parts - list of sprite objects
-        objects.append(pyglet.sprite.Sprite(green_image, x * SQUARE_SIZE[0], y * SQUARE_SIZE[1], batch=batch))
+        objects.append(pyglet.sprite.Sprite(snake_tiles['end-end'], x * SQUARE_SIZE[0], y * SQUARE_SIZE[1], batch=batch))
 
     # FOOD
-    objects.append(pyglet.sprite.Sprite(apple_image, my_snake.apple_position[0] * SQUARE_SIZE[0],
+    objects.append(pyglet.sprite.Sprite(snake_tiles['apple'], my_snake.apple_position[0] * SQUARE_SIZE[0],
                                         my_snake.apple_position[1] * SQUARE_SIZE[1], batch=batch))
 
     return batch
